ca4031c1a2e5c4121aa56771a74cca234736dca3
Chris Hayes <chris@moderncognition.com>
2015-07-21 14:29:06 -0400

    Add comments to filter bag class

--- src/FilterBag.php
+++ src/FilterBag.php
@@ -4,16 +4,35 @@ use Closure;
 
 class FilterBag {
 
-	protected $filters = [];
+	/**
+	 * @var array
+	 */
+	private $filters = [];
 
+	/**
+	 * Return all of the filters currently stored in this filter bag.
+	 * 
+	 * @return array
+	 */
 	public function getFilters() {
 		return $this->filters;
 	}
 
+	/**
+	 * Add a filter to this filter bag.
+	 * 
+	 * @param string
+	 * @param Closure
+	 */
 	public function addFilter($name, Closure $filter) {
 		$this->filters[$name] = $filter;
 	}
 
+	/**
+	 * Remove a filter from this filter bag.
+	 * 
+	 * @param  string
+	 */
 	public function removeFilter($name) {
 		unset($this->filters[$name]);
 	}
